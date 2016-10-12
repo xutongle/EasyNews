e627cef14f511c32bf25eb7e60906296dbbe7ad2
mac_zly <a781683161@qq.com>
2016-10-12 20:29:54 +0800

    安装唯一一个第三方snapkit

---
+++ EasyWeather-Version3/Pods/Target Support Files/Pods-EasyWeather-Version3/Pods-EasyWeather-Version3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EasyWeather_Version3 : NSObject
+@end
+@implementation PodsDummy_Pods_EasyWeather_Version3
+@end
---
+++ EasyWeather-Version3/Pods/Target Support Files/Pods-EasyWeather-Version3/Pods-EasyWeather-Version3-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_EasyWeather_Version3VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EasyWeather_Version3VersionString[];
+
