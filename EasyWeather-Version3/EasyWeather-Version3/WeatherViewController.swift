44a262aa1740caa078acda599591f59b75fa6bef
mac_zly <a781683161@qq.com>
2016-10-12 20:42:00 +0800

    ReInit

---
+++ EasyWeather-Version3/EasyWeather-Version3/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  EasyWeather-Version3
+//
+//  Created by mac_zly on 2016/10/12.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
