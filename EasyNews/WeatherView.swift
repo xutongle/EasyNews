c7b8638fcb36d65a324017c4dd358377e090e4ae
mac_zly <a781683161@qq.com>
2016-12-09 13:55:49 +0800

    Swift News

---
+++ EasyNews/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  EasyNews
+//
+//  Created by mac_zly on 2016/12/9.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ EasyNews/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherViewController.swift
+//  EasyNews
+//
+//  Created by mac_zly on 2016/12/9.
+//  Copyright © 2016年 zly. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        
+        
+    }
+
+}
